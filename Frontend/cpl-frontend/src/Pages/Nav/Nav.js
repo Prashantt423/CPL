8402208f3b28bcfd7437c8f567011661cc7b3bb6
NishantPatel27 <143695937+NishantPatel27@users.noreply.github.com>
2023-09-20 20:23:49 +0530

    #added icons and made some little changes

--- Frontend/cpl-frontend/src/Pages/Nav/Nav.js
+++ Frontend/cpl-frontend/src/Pages/Nav/Nav.js
@@ -1,24 +1,37 @@
 import "./Nav.css";
+import logo from "./logo.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTableColumns } from "@fortawesome/free-solid-svg-icons";
+import { faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faMoneyBills } from "@fortawesome/free-solid-svg-icons";
+
 
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const dashIcon = <FontAwesomeIcon icon={faTableColumns}/>
+  const peopeIcon = <FontAwesomeIcon icon={faPeopleGroup}/>
+  const teamIcon = <FontAwesomeIcon icon={faUser}/>
+  const auctionIcon = <FontAwesomeIcon icon={faMoneyBills}/>
+
   return (
     <div className="navbar">
-      <img src="/public/assets/images/logo.png" alt="logo" />
+      <img src={logo} alt="logo" />
       <nav>
         <ul>
           <li>
-            <Link to="/">Dashboard</Link>
+            <Link to="/">{dashIcon} Dashboard</Link>
           </li>
           <li>
-            <Link to="/players">Players</Link>
+            <Link to="/players">{teamIcon} Players</Link>
           </li>
           <li>
-            <Link to="/teams">Teams</Link>
+            <Link to="/teams">{peopeIcon} Teams</Link>
           </li>
           <li>
-            <Link to="/auction">Auction</Link>
+            <Link to="/auction">{auctionIcon} Auction</Link>
+    
           </li>
         </ul>
       </nav>
--- Frontend/cpl-frontend/src/Pages/Seachbar/Searchbar.js
+++ Frontend/cpl-frontend/src/Pages/Seachbar/Searchbar.js
@@ -1,9 +1,12 @@
 import "./search.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Searchbar = () => {
+  const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass}/>
   return (
     <div className="search-bar">
-      <input type="text" placeholder="Search" />
+      <input type="text"  placeholder= "Search "/> <button className="search-button">{searchIcon}</button>
     </div>
   );
 };
