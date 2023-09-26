aacd6ff529fdd7c76b11d68179e4a119ee4c545c
Meezan Mallick <79187156+meezan-primaryitcare@users.noreply.github.com>
2023-09-26 18:15:12 +0530

    add player form UI improved

--- Frontend/cpl-frontend/src/Pages/Players/add player/Form.js
+++ Frontend/cpl-frontend/src/Pages/Players/add player/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./form.css";
-import axios from "axios"
+import axios from "axios";
 
 const Form = () => {
   const [formData, setFormData] = useState({
@@ -13,7 +13,7 @@ const Form = () => {
     baseprice: "",
     bidprice: "",
     previousteam: "Knights",
-    curruntteam: "Knights",
+    currentteam: "Knights",
     playertype: "batsman",
     totalruns: "",
     innings: "",
@@ -35,21 +35,22 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
-    const formDataJson = JSON.stringify(formData)
-    axios.post("http://localhost:6001/player/add" , formDataJson ,{
-      headers:{
-        "Content-Type" : "application/json"
-      }
-    })
-    .then(result => console.log(result))
-    .catch(err => console.log(err))
-  }
+    const formDataJson = JSON.stringify(formData);
+    axios
+      .post("http://localhost:6001/player/add", formDataJson, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
         <div className="header">
-          <div className="heading">
-            <h1>Add Player</h1>
+          <div>
+            <h2>Add Player</h2>
           </div>
           <div className="submit-btn">
             <button className="submit" type="submit">
@@ -57,12 +58,13 @@ const Form = () => {
             </button>
           </div>
         </div>
-        <div className="personal-details">
-          <h2>Personal Details</h2>
-          <div className="col-1">
-            <div className="name">
+
+        {/* PERSONAL DETAILS */}
+        <div className="form-container">
+          <div className="row">
+            <div>
               <label htmlFor="playername">Player Name</label>
-              <br />
+
               <input
                 type="text"
                 name="playername"
@@ -72,9 +74,9 @@ const Form = () => {
                 onChange={handleInputChange}
               />
             </div>
-            <div className="mob-num">
+            <div>
               <label htmlFor="mobilenumber">Mobile Number</label>
-              <br />
+
               <input
                 type="text"
                 name="mobilenumber"
@@ -84,12 +86,12 @@ const Form = () => {
                 onChange={handleInputChange}
               />
             </div>
-            <div className="semester">
+            <div>
               <label htmlFor="curruntsemester">Current Semester</label>
-              <br />
+
               <select
                 name="curruntsemester"
-                id="sem"
+                className="dropdown"
                 value={formData.curruntsemester}
                 onChange={handleInputChange}
               >
@@ -99,201 +101,133 @@ const Form = () => {
               </select>
             </div>
           </div>
-          <div className="col-2">
-            <div className="dob">
-              <label htmlFor="dateofbirth">Date of Birth</label>
-              <br />
+
+          <div className="row">
+            <div>
+              <label htmlFor="DOB">Date of Birth</label>
               <input
                 type="date"
                 name="dateofbirth"
-                id="dateofbirth"
-                placeholder="Select date"
+                id="DOB"
                 value={formData.dateofbirth}
                 onChange={handleInputChange}
               />
             </div>
-            <div className="branch-details">
-              <label htmlFor="branchname">Branch</label>
-              <br />
+            <div>
+              <label htmlFor="branch">Branch</label>
+
               <select
-                name="branchname"
+                name="branch"
+                className="dropdown"
                 value={formData.branchname}
                 onChange={handleInputChange}
               >
-                <option value="software">Software Development Web And mobile</option>
-                <option value="it/ims">IT/IMS</option>
+                <option value="software">Software</option>
+                <option value="itims">ITIMS</option>
                 <option value="animation">Animation</option>
-                <option value="cyber-security">Cyber Security</option>
-                <option value="cloud computing">Cloud Computing</option>
+                <option value="digital design">Digital Desing</option>
               </select>
             </div>
-            <div className="photo">
-              <label htmlFor="playerphoto">Player's Photo</label>
-              <br />
+            <div>
+              <label htmlFor="playerphoto">Players Photo</label>
               <input
-                type="file"
-                accept="image/*"
-                id="playerphoto"
                 name="playerphoto"
+                type="file"
+                value={formData.playerphoto}
                 onChange={handleInputChange}
               />
             </div>
           </div>
         </div>
-        <div className="auction">
-          <h2>Auction</h2>
-          <div className="col-3">
-            <div className="base-price">
+
+        {/* AUCTION */}
+
+        <div className="form-container auction">
+          <div className="row">
+            <div>
               <label htmlFor="baseprice">Base Price</label>
-              <br />
+
               <input
-                type="text"
+                type="number"
+                min={0}
                 name="baseprice"
+                id="baseprice"
                 placeholder="Base price"
                 value={formData.baseprice}
                 onChange={handleInputChange}
               />
             </div>
-            <div className="bid-price">
-              <label htmlFor="bidprice">Bid Price</label>
-              <br />
+            <div>
+              <label htmlFor="baseprice">Bid Price</label>
+
               <input
-                type="text"
+                type="number"
+                min={0}
                 name="bidprice"
+                id="bidprice"
                 placeholder="Bid price"
                 value={formData.bidprice}
                 onChange={handleInputChange}
               />
             </div>
-            <div className="prev-team">
+            <div>
               <label htmlFor="previousteam">Previous Team</label>
-              <br />
+
               <select
                 name="previousteam"
-                value={formData.previousteam}
+                className="dropdown"
+                value={formData.curruntsempreviousteamester}
                 onChange={handleInputChange}
               >
                 <option value="Knights">Knights</option>
-                <option value="hurricanes">Hurricanes</option>
-                <option value="royals">Royals</option>
-                <option value="blasters">Blasters</option>
-                <option value="stars">Stars</option>
-                <option value="panthers">Panthers</option>
+                <option value="Hurricanes">Hurricanes</option>
+                <option value="Royals">Royals</option>
+                <option value="Blasters">Blasters</option>
+                <option value="Panther">Panther</option>
+                <option value="Empire">Empire</option>
+                <option value="Wolves">Wolves</option>
+                <option value="Super Kings">Super Kings</option>
+                <option value="Striker">Striker</option>
+                <option value="Titans">Titans</option>
+                <option value="Falcons">Falcons</option>
               </select>
             </div>
-            <div className="currunt-team">
-              <label htmlFor="curruntteam">Current Team</label>
-              <br />
+            <div>
+              <label htmlFor="currentteam">Current Team</label>
+
               <select
-                name="curruntteam"
-                value={formData.curruntteam}
+                name="currentteam"
+                className="dropdown"
+                value={formData.currentteam}
                 onChange={handleInputChange}
               >
                 <option value="Knights">Knights</option>
-                <option value="hurricanes">Hurricanes</option>
-                <option value="royals">Royals</option>
-                <option value="blasters">Blasters</option>
-                <option value="stars">Stars</option>
-                <option value="panthers">Panthers</option>
+                <option value="Hurricanes">Hurricanes</option>
+                <option value="Royals">Royals</option>
+                <option value="Blasters">Blasters</option>
+                <option value="Panther">Panther</option>
+                <option value="Empire">Empire</option>
+                <option value="Wolves">Wolves</option>
+                <option value="Super Kings">Super Kings</option>
+                <option value="Striker">Striker</option>
+                <option value="Titans">Titans</option>
+                <option value="Falcons">Falcons</option>
               </select>
             </div>
-            <div className="player-type">
+
+            <div>
               <label htmlFor="playertype">Player Type</label>
-              <br />
+
               <select
                 name="playertype"
+                className="dropdown"
                 value={formData.playertype}
                 onChange={handleInputChange}
               >
                 <option value="batsman">Batsman</option>
-                <option value="bowler">Bowler</option>
+                <option value="bolwer">Bowler</option>
                 <option value="allrounder">All Rounder</option>
               </select>
-            </div>
-          </div>
-        </div>
-        <div className="statistics">
-          <h2>Statistics</h2>
-          <div className="col-4">
-            <div className="total-runs">
-              <label htmlFor="totalruns">Total Runs</label>
-              <br />
-              <input
-                type="text"
-                name="totalruns"
-                placeholder="Total runs"
-                value={formData.totalruns}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="innings">
-              <label htmlFor="innings">Innings</label>
-              <br />
-              <input
-                type="text"
-                name="innings"
-                placeholder="Innings"
-                value={formData.innings}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="strike-rate">
-              <label htmlFor="strikerate">Strike Rate</label>
-              <br />
-              <input
-                type="text"
-                name="strikerate"
-                placeholder="Strike rate"
-                value={formData.strikerate}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="average">
-              <label htmlFor="averageruns">Average</label>
-              <br />
-              <input
-                type="text"
-                name="averageruns"
-                placeholder="Average"
-                value={formData.averageruns}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="total-wickets">
-              <label htmlFor="totalwickets">Total Wickets</label>
-              <br />
-              <input
-                type="text"
-                name="totalwickets"
-                placeholder="Total wickets"
-                value={formData.totalwickets}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="col-5">
-            <div className="average-b">
-              <label htmlFor="averagebowling">Average</label>
-              <br />
-              <input
-                type="text"
-                name="averagebowling"
-                placeholder="Average"
-                value={formData.averagebowling}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="eco">
-              <label htmlFor="economy">Economy</label>
-              <br />
-              <input
-                id="economy"
-                type="text"
-                name="economy"
-                placeholder="Economy"
-                value={formData.economy}
-                onChange={handleInputChange}
-              />
             </div>
           </div>
         </div>
@@ -303,4 +237,3 @@ const Form = () => {
 };
 
 export default Form;
-
